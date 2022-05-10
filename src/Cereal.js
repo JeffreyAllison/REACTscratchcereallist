2c12e3ceeb987f6def8917b715cb10b55505bc41
JeffAllison <itsjallison@gmail.com>
2022-05-10 16:22:40 -0700

    created function cereal to render cereal list item in cereal.js

--- src/Cereal.js
+++ src/Cereal.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Cereal() {
-  return <div>Cereal</div>;
+export default function Cereal({ cereal }) {
+  return (
+    <div className="cereal-list-item">
+      <Link to={`/cereals/${cereal.id}`}>
+        <h3>Name: {cereal.name}</h3>
+        <p>Cups of cereal per serving: {cereal.cups}</p>
+        <p>Calories per serving: {cereal.calories}</p>
+        <p>grams per serving...</p>
+        <p>Sugar: {cereal.sugar}</p>
+        <p>Protein: {cereal.protein}</p>
+        <p>Fat: {cereal.fat}</p>
+        <p>Sodium: {cereal.sodium}</p>
+        <p>Fiber: {cereal.fiber}</p>
+        <p>Carbohydrates: {cereal.carbo}</p>
+        <p>Potassium: {cereal.potass}</p>
+      </Link>
+    </div>
+  );
 }
--- src/CerealsList.js
+++ src/CerealsList.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import Cereal from './Cereal';
 
-export default function CerealsList() {
-  return <div>CerealsList</div>;
+export default function CerealsList({ cereals }) {
+  return (
+    <div className="cereals">
+      {cereals.map((cereal) => (
+        <Cereal key={cereal.id} cereal={cereal} />
+      ))}
+    </div>
+  );
 }
